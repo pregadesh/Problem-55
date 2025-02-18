1d7007d7a8e6ef92ac4769458b49d070f282b304
Pregadesh <144701889+pregadesh@users.noreply.github.com>
2025-02-18 13:47:50 +0530

    Arraysandstring.java
    First topic

---
+++ Arraysandstring.java
@@ -0,0 +1,12 @@
+// Arrays and String
+//1. Largest element and smallest element 
+import java.util.Arrays;
+class Main{
+  public static void main (String[] args){
+      int[] arr = {0,2,3,4,5,6};
+      Arrays.sort(arr);
+      int n = arr.length;
+      System.out.print("Lowest element : "+arr[0] + "  Highest element :" + arr[n-1); // Lowest element : 0 Highest element : 6
+  }
+}
+// 2. Reverse a String without an lib function
